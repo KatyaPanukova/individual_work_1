707549b59e97e96777af634cf6364b068014e7e5
KatyaPanukova <55399352+KatyaPanukova@users.noreply.github.com>
2020-03-11 15:36:51 +0300

    Update fir_1.py

--- fir_1.py
+++ fir_1.py
@@ -1,18 +1,15 @@
-# бинарный поиск через цикл
 import time
 from random import randint
-import sys
-sys.setrecursionlimit(1000000)
 
 list_of_nums = []
 count_element = int(input('Input quantity of elements :'))
-for i in range(count_element):
-    list_of_nums.append(randint(-1000, 1000))
-list_sorted = sorted(list_of_nums)
-print(list_sorted)
 
+for i in range(count_element):
+    list_of_nums.append(randint(-100000, 100000))
+list_sorted = sorted(list_of_nums)
 desired_num = int(input('Input desired number :'))
 n = 0
+# бинарный поиск через цикл
 while count_element != 0:
     start = time.time()
     if desired_num != list_sorted[n]:
@@ -26,21 +23,36 @@ else:
     print('None')
 # бинарный поиск через рекурсию
 
-def function(list_sorted,desired_num,a = 0):
-    if list_sorted == 0:
-        return None
-    if list_sorted[0] == desired_num:
-        return a
-    return function(list_sorted[1:], desired_num, a + 1)
+a = list_sorted[0]
+b = list_sorted[-1]
+
+
+def function(list_sorted,desired_num, a, b):
+    if b >= a:
+        c = a + (b - a) // 2
+        if list_sorted[c] == desired_num:
+            return c
+        elif list_sorted[c] > desired_num:
+            return function(list_sorted, desired_num, a, c - 1)
+        return function(list_sorted, desired_num, c + 1, b)
+    return None
+
 
 def main():
     start_1 = time.time()
-    print('__________________________________')
-    print('Index of num {}'.format(desired_num), ':', function(list_sorted,desired_num))
-    print('Time of recursive programs work: ', str(time.time()-start_1) + 'sec.')
+
+    if function(list_sorted, desired_num,  0, len(list_sorted) - 1) == None:
+
+        print('__________________________________')
+        print(function(list_sorted, 0, len(list_sorted) - 1, desired_num))
+    else:
+        print('__________________________________')
+        print('Index of num {}'.format(desired_num), ':', function(list_sorted, desired_num,  0, len(list_sorted) - 1))
+        print('Time of recursive programs work: ', str(time.time()-start_1) + 'sec.')
 try:
     main()
 except MemoryError:
     print('Limit is exceeded!')
 except IndexError:
+    print('__________________________________')
     print('None')
