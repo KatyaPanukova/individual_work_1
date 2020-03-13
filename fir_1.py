567906e61ef1e3cf07b3f3104be1aabc75811631
KatyaPanukova <55399352+KatyaPanukova@users.noreply.github.com>
2020-03-13 15:34:00 +0300

    Update fir_1.py

--- fir_1.py
+++ fir_1.py
@@ -5,54 +5,48 @@ list_of_nums = []
 count_element = int(input('Input quantity of elements :'))
 
 for i in range(count_element):
-    list_of_nums.append(randint(-100000, 100000))
+    list_of_nums.append(randint(-1000, 1000))
 list_sorted = sorted(list_of_nums)
+print(list_sorted)
 desired_num = int(input('Input desired number :'))
 n = 0
-# бинарный поиск через цикл
-while count_element != 0:
+
+# итеративный способ
+def f(count_element,n):
+    while count_element != 0:
+        if desired_num != list_sorted[n]:
+            count_element -= 1
+        elif desired_num == list_sorted[n]:
+            return n
+        n += 1
+    else:
+        print('None')
+def main_1():
     start = time.time()
-    if desired_num != list_sorted[n]:
-        count_element -= 1
-    elif desired_num == list_sorted[n]:
-        print('Index of num {}'.format(desired_num), ':', n)
-        print('Time of iterative programs work: ', str(time.time()-start) + 'sec.')
-        break
-    n += 1
-else:
-    print('None')
-# бинарный поиск через рекурсию
+    print('Index of num :', f(count_element, n))
+    print('Time of iterative programs work:', str(time.time() - start) + ' sec.')
+main_1()
 
-a = list_sorted[0]
-b = list_sorted[-1]
+a = 0
+b = len(list_sorted) - 1
 
+# рекурсивный способ
 
 def function(list_sorted,desired_num, a, b):
-    if b >= a:
-        c = a + (b - a) // 2
+    if b < a:
+        return None
+    else:
+        c = (a + b) // 2
         if list_sorted[c] == desired_num:
             return c
-        elif list_sorted[c] > desired_num:
+        elif list_sorted[c] >= desired_num:
             return function(list_sorted, desired_num, a, c - 1)
         return function(list_sorted, desired_num, c + 1, b)
-    return None
 
 
 def main():
     start_1 = time.time()
+    print(function(list_sorted,desired_num, a, b))
+    print('Time of recursive programs work:', str(time.time()-start_1) + ' sec.')
 
-    if function(list_sorted, desired_num,  0, len(list_sorted) - 1) == None:
-
-        print('__________________________________')
-        print(function(list_sorted, 0, len(list_sorted) - 1, desired_num))
-    else:
-        print('__________________________________')
-        print('Index of num {}'.format(desired_num), ':', function(list_sorted, desired_num,  0, len(list_sorted) - 1))
-        print('Time of recursive programs work: ', str(time.time()-start_1) + 'sec.')
-try:
-    main()
-except MemoryError:
-    print('Limit is exceeded!')
-except IndexError:
-    print('__________________________________')
-    print('None')
+main()
